44201cbfeceb92a1d0c61f5a72aa1ac7d1045f17
Christian Camilo Restrepo <ckrestrepo@gmail.com>
2014-11-25 13:22:06 -0500

    Update codigo cliente.py

--- codigo cliente.py
+++ codigo cliente.py
@@ -4,7 +4,7 @@ try:
 	n = int(raw_input("Digite la cantidad de dolares a convertir: "))
 	#Conectamos con el servidor
 	proxy = xmlrpclib.ServerProxy("http://localhost:8000/")
-	print "Cantidad de dolares en pesos (USD): %s" % str(proxy.funcion1(n))
+	print "Cantidad de dolares en pesos $: %s" % str(proxy.funcion1(n))
 	raw_input("Presione una tecla para terminar")
 
 except ValueError:
